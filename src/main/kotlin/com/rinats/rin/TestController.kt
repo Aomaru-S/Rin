90788a50ff1f484d8b8b91b2048ea07b934a83c0
181032 <Souhei-H>
2021-11-01 14:12:09 +0900

    fix application.properties

--- src/main/kotlin/com/rinats/rin/TestController.kt
+++ src/main/kotlin/com/rinats/rin/TestController.kt
@@ -1,2 +1,12 @@
 package com.rinats.rin
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController {
+    @GetMapping("/")
+    fun index(): String {
+        return "hello"
+    }
+}
