98ac5157efd7a93f446174476b6062aae5c98f4d
Roin-hk <ninsin12@gmail.com>
2021-11-02 14:16:50 +0900

    first commit

---
+++ src/main/kotlin/com/rinats/rin/controller/EmployeeController.kt
@@ -0,0 +1,19 @@
+package com.rinats.rin.controller
+
+import com.rinats.rin.model.Employee
+import com.rinats.rin.repository.EmployeeRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class EmployeeController (
+    @Autowired
+    val employeeRepository: EmployeeRepository
+) {
+
+    @GetMapping("/employeeList")
+    fun employeeList(): MutableList<Employee> {
+        return employeeRepository.findAll()
+    }
+}
