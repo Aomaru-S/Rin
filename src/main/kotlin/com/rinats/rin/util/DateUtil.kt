0b3b1aa9430d7804dc97de885f46da225a742321
181032 <Souhei-H>
2021-11-22 14:30:23 +0900

    no comments

--- src/main/kotlin/com/rinats/rin/util/DateUtil.kt
+++ src/main/kotlin/com/rinats/rin/util/DateUtil.kt
@@ -7,13 +7,15 @@ import java.util.*
 class DateUtil {
     companion object {
         fun getDate(year: Int, month: Int, day: Int): Date {
-            val dateFormat = SimpleDateFormat("yyyyMMdd")
-            dateFormat.isLenient = false
+
             val sYear = year.toString().padStart(4, '0')
             val sMonth = month.toString().padStart(2, '0')
             val sDay = day.toString().padStart(2, '0')
             val sDate = "$sYear$sMonth$sDay"
+
             try {
+                val dateFormat = SimpleDateFormat("yyyyMMdd")
+                dateFormat.isLenient = false
                 return dateFormat.parse(sDate)
             } catch (e: ParseException) {
                 throw ParseException("Can not parse $sDate to java.util.Date", 0)
