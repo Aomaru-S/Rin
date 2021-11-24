ee6585bd5e47d53b0b1be0f5f562aac1449cbe53
181032 <Souhei-H>
2021-11-24 17:17:29 +0900

    no comments

--- src/main/kotlin/com/rinats/rin/controller/parttimejob/EmployeeRestController.kt
+++ src/main/kotlin/com/rinats/rin/controller/parttimejob/EmployeeRestController.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestAttribute
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@RestController("api/v1/employee")
 class EmployeeRestController(
     @Autowired
     private val employeeService: EmployeeService,
