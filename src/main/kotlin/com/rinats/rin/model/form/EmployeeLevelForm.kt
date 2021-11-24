00e805ffbc56db2f5c921037ab4cdbde50acdc73
Roin-hk <ninsin12@gmail.com>
2021-11-24 10:37:26 +0900

    employeeLevel commits

---
+++ src/main/kotlin/com/rinats/rin/model/form/EmployeeLevelForm.kt
@@ -0,0 +1,13 @@
+package com.rinats.rin.model.form
+
+import javax.validation.constraints.Max
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotBlank
+
+data class EmployeeLevelForm(
+    @field:NotBlank
+    var id: String? = null,
+    @Min(1)
+    @Max(5)
+    var level: Int? = null
+)
