21d01d398afc9cb30764343244924663aeed206b
181032 <Souhei-H>
2021-12-08 16:59:30 +0900

    no comments

--- src/main/kotlin/com/rinats/rin/controller/parttimejob/PartTimeJobController.kt
+++ src/main/kotlin/com/rinats/rin/controller/parttimejob/PartTimeJobController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import javax.servlet.http.HttpServletRequest
 
 @Controller
-class PartTimeJobRestController {
+class PartTimeJobController {
     @PartTimeJob
     @GetMapping("/part_time_job_top")
     fun partTimeJobTop(request: HttpServletRequest): String {
