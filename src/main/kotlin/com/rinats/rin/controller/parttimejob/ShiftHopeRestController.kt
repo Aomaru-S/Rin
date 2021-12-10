874008f1544e96c02fa2af3a6d9b8954f8c90278
181032 <Souhei-H>
2021-12-10 12:21:51 +0900

    no comments

--- src/main/kotlin/com/rinats/rin/controller/parttimejob/ShiftHopeRestController.kt
+++ src/main/kotlin/com/rinats/rin/controller/parttimejob/ShiftHopeRestController.kt
@@ -9,6 +9,7 @@ import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.*
 
 @RestController
+@RequestMapping("/api/shift_hope")
 class ShiftHopeRestController(
     @Autowired
     private val shiftHopeService: ShiftHopeService
