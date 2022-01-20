5179ff861e17b680b47ea2e961323bbafff10877
181032 <Souhei-H>
2022-01-20 12:05:36 +0900

    no comments

--- src/main/kotlin/com/rinats/rin/service/ShiftHopeService.kt
+++ src/main/kotlin/com/rinats/rin/service/ShiftHopeService.kt
@@ -39,7 +39,7 @@ class ShiftHopeService(
     fun submitShift(shiftHopeForm: ShiftHopeForm, employeeId: String): Boolean {
         val year = shiftHopeForm.year ?: return false
         val month = shiftHopeForm.month ?: return false
-        shiftHopeForm.days ?: return false
+        shiftHopeForm.days ?: return true
 
         shiftHopeForm.days.forEach { day ->
             val date = try {
