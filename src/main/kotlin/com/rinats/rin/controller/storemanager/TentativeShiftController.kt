dbf968777996a3747684a9453307f3271866b9d4
181032 <Souhei-H>
2022-02-16 10:53:48 +0900

    no comments

--- src/main/kotlin/com/rinats/rin/controller/storemanager/TentativeShiftController.kt
+++ src/main/kotlin/com/rinats/rin/controller/storemanager/TentativeShiftController.kt
@@ -2,7 +2,6 @@ package com.rinats.rin.controller.storemanager
 
 import com.rinats.rin.model.other.CompleteMessage
 import com.rinats.rin.model.table.Employee
-import com.rinats.rin.service.EmployeeService
 import com.rinats.rin.service.ShiftGeneratorService
 import com.rinats.rin.service.TentativeShiftService
 import org.springframework.beans.factory.annotation.Autowired
@@ -26,7 +25,31 @@ class TentativeShiftController(
         model: Model
     ): String {
         val calendar = Calendar.getInstance()
+        calendar.isLenient = false
+        calendar.add(Calendar.MONTH, 1)
         val dayCount = calendar.getActualMaximum(Calendar.DAY_OF_MONTH)
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH) + 1
+        calendar.set(Calendar.DAY_OF_MONTH, 1)
+        var firstDayOfWeek = calendar.get(Calendar.DAY_OF_WEEK)
+        val dayOfWeekMap = mutableMapOf<Int, String>()
+        for (i in 1..7) {
+            dayOfWeekMap[i] = when(firstDayOfWeek) {
+                1 -> "土"
+                2 -> "日"
+                3 -> "月"
+                4 -> "火"
+                5 -> "水"
+                6 -> "木"
+                7 -> "金"
+                else -> ""
+            }
+            firstDayOfWeek++
+            if (firstDayOfWeek == 8) {
+                firstDayOfWeek -= 7
+            }
+        }
+
         val tentativeShiftMap = mutableMapOf<Employee, MutableList<Int>>()
         tentativeShiftService.getTentativeShiftList().forEach {
             if (tentativeShiftMap.containsKey(it.id?.employee)) {
@@ -35,6 +58,9 @@ class TentativeShiftController(
                 tentativeShiftMap[it.id?.employee!!] = mutableListOf(it.id?.shiftDate!!.dayOfMonth)
             }
         }
+        model.addAttribute("year", year)
+        model.addAttribute("month", month)
+        model.addAttribute("dayOfWeekMap", dayOfWeekMap)
         model.addAttribute("tentativeShiftList", tentativeShiftMap)
         model.addAttribute("dayCount", dayCount)
         return "tentative_shift_index"
@@ -97,4 +123,5 @@ class TentativeShiftController(
         tentativeShiftService.getTentativeShiftList()
         return "complete"
     }
+
 }
